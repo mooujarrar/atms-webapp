f0fd62f701666866865153db3150c957fc167626
Mohyiddine <mohyiddine.oujarrar@siemens.com>
2023-06-23 18:06:17 +0200

    enh# Add bootstrap and background effect

---
+++ src/components/Background.tsx
@@ -0,0 +1,17 @@
+
+import './Background.scss'
+
+function Background() {
+    return (
+        <div className="hero">
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+            <div className="cube"></div>
+        </div>
+    );
+}
+
+export default Background;
---
+++ src/components/Content.tsx
@@ -0,0 +1,12 @@
+
+import './Content.scss'
+
+function Content() {
+    return (
+        <div className="content">
+            Test
+        </div>
+    );
+}
+
+export default Content;
