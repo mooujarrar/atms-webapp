54fd750f40005021aa7cf81daaa75f8369ff69c8
Mohyiddine <mohyiddine.oujarrar@siemens.com>
2023-07-24 17:44:30 +0200

    enh# webapp enhanced, add the reset entries button, correct layout, introduce icons

---
+++ src/data/fake_sink.ts
@@ -0,0 +1,129 @@
+import { IEntry } from "../model/Entry";
+
+export const FAKE_ENTRIES: IEntry[] = [
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 1,
+        date: 1690168136
+    },
+    {
+        tag: "tag2",
+        direction: 0,
+        date: 1690168140
+    },
+    {
+        tag: "tag2",
+        direction: 0,
+        date: 1690168080
+    },
+    {
+        tag: "tag3",
+        direction: 0,
+        date: 1690167446
+    },
+    {
+        tag: "tag3",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 1,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag3",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 1,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag3",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 1,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag3",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 1,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag3",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 1,
+        date: 1690168135
+    },
+    {
+        tag: "tag1",
+        direction: 0,
+        date: 1690168135
+    },
+]
