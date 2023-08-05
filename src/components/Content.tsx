715067531223c3e8a36a2eefddba44a10315019c
mooujarrar <mohyiddine1997@gmail.com>
2023-08-05 20:01:17 +0200

    enh#Insert icons for In Out and remove the log

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,12 @@
-import { Table, Navbar, Container, Badge, Spinner, Button } from 'react-bootstrap';
-import { BiWifiOff } from "@react-icons/all-files/bi/BiWifiOff";
+import Table from 'react-bootstrap/Table';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Badge from 'react-bootstrap/Badge';
+import Spinner from 'react-bootstrap/Spinner';
+import Button from 'react-bootstrap/Button';
+import { AiOutlineDisconnect } from "@react-icons/all-files/ai/AiOutlineDisconnect";
+import { ImEnter } from "@react-icons/all-files/im/ImEnter";
+import { ImExit } from "@react-icons/all-files/im/ImExit";
 import './Content.scss'
 import logo from '../assets/app-icon.png'; // Tell webpack this JS file uses this image
 import { useCallback, useEffect, useState } from 'react';
@@ -14,11 +21,10 @@ function Content() {
     const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
     const isFake = false;
     useEffect(() => {
-        console.log('**** Message', lastMessage);
         if (lastMessage !== null) {
             setEntries((JSON.parse(lastMessage.data) as IEntry[]).sort((a, b) => (a.date > b.date ? -1 : 1)));
         }
-        if(isFake) {
+        if (isFake) {
             setEntries(FAKE_ENTRIES.sort((a, b) => (a.date > b.date ? -1 : 1)));
         }
     }, [lastMessage, setEntries, isFake]);
@@ -51,7 +57,7 @@ function Content() {
             <div className="content d-flex flex-column">
                 {/*<div className="status">Connection state to the Hardware: {connectionStatus}</div>*/}
                 <div className="table-container">
-                    { (readyState === ReadyState.OPEN || isFake) ?
+                    {(readyState === ReadyState.OPEN || isFake) ?
                         <div style={{ width: "100%" }}>
                             <Table striped bordered variant="dark">
                                 <thead>
@@ -69,7 +75,10 @@ function Content() {
                                                 <td>{index}</td>
                                                 <td>{listValue.tag}</td>
                                                 <td>{new Date(listValue.date * 1000).toLocaleDateString()} - {new Date(listValue.date * 1000).toLocaleTimeString()}</td>
-                                                <td>{listValue.direction}</td>
+                                                <td>{listValue.direction === 0 ?
+                                                    <ImEnter style={{ width: "48px", height: "48px" }} />
+                                                    : <ImExit style={{ width: "48px", height: "48px" }} />}
+                                                </td>
                                             </tr>
                                         );
                                     })}
@@ -88,7 +97,7 @@ function Content() {
                                         role="status"
                                         aria-hidden="true"
                                         variant="primary"
-                                    /> : <BiWifiOff style={{ color: "#B71C1C" }} size="3em" />
+                                    /> : <AiOutlineDisconnect style={{ color: "#B71C1C" }} size="3em" />
                             }
                             <span style={{ color: readyState === ReadyState.CLOSED ? "#B71C1C" : "white", fontWeight: "bold" }}>{connectionStatus}</span>
                         </div>
